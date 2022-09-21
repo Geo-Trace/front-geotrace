6d735e8d4e1ae8ae9b3c2ce5a7919e278549ee39
nacimh <haiahem.nacim@gmail.com>
2022-09-21 23:14:25 +0200

    feat: mise en forme des 'card' infos :lipstick:

--- src/components/ItineraireInfos.tsx
+++ src/components/ItineraireInfos.tsx
@@ -1,29 +1,66 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Itineraire from '../models/Itineraire';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import { Container, Grid} from "@mui/material";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Itineraire from "../models/Itineraire";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import { Container, Grid } from "@mui/material";
+import { PassThrough } from "stream";
 
-type Props = { 
-  itineraire: Itineraire
-}
+type Props = {
+  itineraire: Itineraire;
+};
 
 const ItineraireInfos: React.FC = () => {
-
-
   return (
-  // <Container>
-  //   <Grid container spacing={2}>
-  //       <Grid item xs={2}>
-  //         <----/>
-  //       </Grid>
-  //   </Grid>
-  // </Container>
-  );
-}
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <Card>
+            <Typography color="text.secondary" gutterBottom>
+              4/5
+            </Typography>
+          </Card>
+        </Grid>
 
-export default ItineraireInfos; 
+        <Grid item xs={2}>
+          <Card>
+            <Typography color="text.secondary" gutterBottom>
+            ↑ 3%
+            </Typography>
+          </Card>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Card>
+            <Typography color="text.secondary" gutterBottom>
+              15 km
+            </Typography>
+          </Card>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Card>
+            <Typography color="text.secondary" gutterBottom>
+              2h30
+            </Typography>
+          </Card>
+        </Grid>
+
+        <Grid item xs={2}>
+          
+        </Grid>
+
+        <Grid item xs={2}>
+          <Card>
+            <Typography color="text.secondary" gutterBottom>
+            ★★★✩✩
+            </Typography>
+          </Card>
+        </Grid>
+      </Grid>
+  );
+};
+
+export default ItineraireInfos;
