b55e3a7867eb77a88c1ef32902d833fe96bc1437
Rémi PONCHE <ponche62880@gmail.com>
2022-10-02 11:07:12 +0200

    chore: ajout redirection au click de la vignette :construction:

--- src/pages/ItineraireList.tsx
+++ src/pages/ItineraireList.tsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 import ItineraireCard from "../components/ItineraireCard"
 import Itineraire from "../models/Itineraire";
 import ITINERAIRES from "../models/mock-itineraire.json";
+import { useNavigate } from 'react-router-dom'; 
 
 
 
@@ -14,13 +15,18 @@ function ItineraireList() {
         // code executer au lancement de la page 
             setItineaires(ITINERAIRES);
     },[]);
+    const navigate = useNavigate(); 
+
+    const gotToItineraire = (id: number) => {
+        navigate(`/itineraire/${id}`); 
+    }
     
     return (
             <Container sx={{ py: 8 }} >
                 <Grid container spacing={4}>
                     {itineraires.map((itineraire) => (
-                        <Grid item key={itineraire.id} xs={12} sm={6} md={4}>
-                            <ItineraireCard itineraire={itineraire} />
+                        <Grid item key={itineraire.id} xs={12} sm={6} md={4} onClick={() => gotToItineraire(itineraire.id)} >
+                            <ItineraireCard itineraire={itineraire}  />
                         </Grid>
                     ))}
                 </Grid>
