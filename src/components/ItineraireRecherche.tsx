95656f75c4e117ce8f45e002209b8f99e85340d1
nacim.h <haiahem.nacim@gmail.com>
2022-10-05 16:26:28 +0200

    fix: indentation code

--- src/components/ItineraireRecherche.tsx
+++ src/components/ItineraireRecherche.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {TextField } from '@mui/material';
+import {Box, TextField } from '@mui/material';
 import Itineraire from '../models/Itineraire';
 import { lightGreen } from '@mui/material/colors';
 
@@ -9,9 +9,16 @@ const ItineraireRecherche: React.FC = () => {
 
 
     return (
-        <form noValidate autoComplete="on">
-        <TextField id="outlined-basic" label="Rechercher un itineraire" variant="outlined" color ="success" placeholder="Placeholder" sx={{mt:2,mb: 5, width: 800}}/>
-      </form>
+      <Box sx = {{mb :5}}>
+        <TextField 
+          fullWidth 
+          id="outlined-basic" 
+          label="Rechercher un itineraire" 
+          variant="outlined" 
+          color ="success" 
+          placeholder="nom, ville, region..."
+        />
+      </Box>
     );
     }
 
