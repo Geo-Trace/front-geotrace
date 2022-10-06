df83efde2dd9eb26870cdaf7dcc97a3ac3191c53
Rémi PONCHE <ponche62880@gmail.com>
2022-10-06 15:01:53 +0200

    chore: mise a jour de l'interface et des props :label:

--- src/components/ItineraireDescriptif.tsx
+++ src/components/ItineraireDescriptif.tsx
@@ -9,12 +9,12 @@ type Props = {
   itineraire: Itineraire
 }
 
-const ItineraireDescriptif: React.FC = () => {
+const ItineraireDescriptif: React.FC<Props> = ({itineraire}) => {
 
 
   return (
         <Typography sx={{padding: 1, mt:8, alignContent: 'center', textAlign: 'justify',fontWeight: 'medium',fontSize: 'h6.fontSize',color: Brown}}>
-          Du hameau des Gourniers (1477m), splendide randonnée en boucle à la journée, en grande partie hors sentier par un itinéraire en crêtes fort esthétique, enchaînant trois sommets : Revire Souléou (2717m), Roche Méane (2651m), le Piarra (2633m). Son caractère aérien et alpin fait que ce parcours, loin d’être de l’alpinisme, est néanmoins une randonnée sérieuse et sportive réservée aux randonneurs confirmés aimant flirter avec le ciel, où rares sont les gens qui s’y aventurent. Musique du vent, solitude et jolies sensations garanties. – Auteur : Thierry GARCIN
+          {itineraire.descriptif}
         </Typography>
   );
 }
